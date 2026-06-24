63175390de85a42f99e7d22244be10aa5a13d6ee
dlzkap11 <dlzkap11@gmail.com>
2026-06-24 09:02:48 +0900

    [Add] 자료구조 프로젝트 추가
    기초 자료구조및 알고리즘 관련 학습 추가 예정

---
+++ DataStructure/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hero";
+            string b = a;
+            b += "!";
+
+            Console.WriteLine(a);
+        }
+    }
+}
