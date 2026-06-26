0d2f4e0b5668c84dc2aa6a423f4c16e8f038fe0c
dlzkap11 <dlzkap11@gmail.com>
2026-06-26 17:34:59 +0900

    [Add] ItemGuide 프로젝트 추가
    너무 많은 일이 있었다 나 너무 힘들었어...
    Dictionary 관련 실습 코드

---
+++ DataStructure/ItemGuide/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItemGuide
+{
+
+    public class Item
+    {
+        // 이름, 뭐 설명
+        public int Id;
+        public string Name;
+        public string Explan;
+        public float Weight;
+        public int MaxDur;
+
+        public Item(int id, string name, string explan, float weight)
+        {
+            Id = id;
+            Name = name;
+            Explan = explan;
+            Weight = weight;
+            MaxDur = 10;
+        }
+
+    }
+
+    public class ItemData
+    {
+        public Item _Item;
+
+        public int Durability = 10;
+    }
+
+}
