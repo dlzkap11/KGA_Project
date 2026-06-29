d22e5cb1b8fbddb2579e78ae770ae0e08e982257
dlzkap11 <dlzkap11@gmail.com>
2026-06-29 16:35:23 +0900

    [Fix] 길찾기 버그 수정, 미로 찾기 리팩토링
    input값이 cnt보다 높을 경우 temp[]배열을 넘어가버리는 버그 수정
    미로찾기 방향배열을 추가하여 반복되는 코드를 함수로 추가

--- DataStructure/Graph/Program.cs
+++ DataStructure/Graph/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         const int MAP_SIZE = 8;
-        const int MIRO_SIZE = 12;
+        const int MIRO_SIZE = 15;
 
         static void MapGraph()
         {
@@ -49,12 +49,16 @@
 
             int input;
             int player = 0;
+
+            //Dictionary<int, int> ChoiceMap = new Dictionary<int, int>();
             int[] temp = new int[MAP_SIZE];
+
+            Console.Clear();
             while (true)
             {
                 int cnt = 0;
                 Console.WriteLine($"현재 위치 : {mapName[player]}");
-
+                
                 for (int Dest = 0; Dest < MAP_SIZE; Dest++)
                 {
                     if (maps[player, Dest])
@@ -67,9 +71,12 @@
                 Console.WriteLine();
                 Console.WriteLine("어디로 이동하시겠습니까?");
                 input = int.Parse(Console.ReadLine());
-                if (input >= MAP_SIZE || input <= 0)
+                if (input == 0)
+                    break;
+                if (input > cnt || input < 0)
                 {
                     Console.WriteLine("미확인 지역입니다.");
+                    Console.WriteLine();
                     continue;
                 }
 
@@ -94,11 +101,12 @@
                     Console.WriteLine($"{mapName[temp[dest]]}으로 이동!");
                     player = temp[dest];
                 }
+                Console.WriteLine();
             }
 
         }
 
-
+        static  char[,] miro = new char[MIRO_SIZE, MIRO_SIZE];
         static void Miro()
         {
             // 미로찾기(12*12)
@@ -110,8 +118,7 @@
             // 플레이어는 상하좌우로 이동가능
             Console.Clear();
 
-            char[,] miro = new char[MIRO_SIZE, MIRO_SIZE];
-
+            // 일단 벽을 세워
             for (int i = 0; i < MIRO_SIZE; i++)
             {
                 for (int j = 0; j < MIRO_SIZE; j++)
@@ -125,6 +132,7 @@
             // 1. 일단 P -> E로 가는 길을 하나 뚫는다.
             // 2. 처음부터 반복문을 돌리면서 이미 뚫린 길' '은 무시 벽'#'은 확률로 길이 뚫림
             // 완성 짠
+
             // P -> E로 가는 길
             // P(0,0) E(11,11) 한칸씩 가면서 뚫기 오른쪽 또는 아래로 뚫기 만약 오른쪽으로 가다가 최대 사이즈에 닿으면 아래로 아래로 갈 때도 최대 사이즈에 닿으면 오른쪽으로 가기 하면 길 뚫린다
             int dotheg_x = 0, dotheg_y = 0;
@@ -135,17 +143,19 @@
                 int i = rand.Next(0, 2);
                 if(i == 0)
                 {
-                    //0이면 오른쪽
+                    //0이면 오른쪽 만약 크기를 넘으면 아래로
                     if (dotheg_x + 1 >= MIRO_SIZE)
-                        continue;
-                    dotheg_x++;
+                        dotheg_y++;
+                    else
+                        dotheg_x++;
                 }
                 else
                 {
-                    //1이면 아래로
+                    //1이면 아래로 만약 크기를 넘으면 오른쪽
                     if (dotheg_y + 1 >= MIRO_SIZE)
-                        continue;
-                    dotheg_y++;
+                        dotheg_x++;
+                    else
+                        dotheg_y++;
                 }
                 miro[dotheg_y, dotheg_x] = ' ';
 
@@ -153,6 +163,7 @@
                     break;
             }
 
+            // 이미 길이 뚫린 곳은 무시(안하면 길이 막힐 수 있다) 아직 벽인 곳은 랜덤으로 길을 뚫는다.
             for (int i = 0; i < MIRO_SIZE; i++)
             {
                 for (int j = 0; j < MIRO_SIZE; j++)
@@ -176,9 +187,6 @@
             miro[player_y, player_x] = 'P';
             miro[MIRO_SIZE - 1, MIRO_SIZE - 1] = 'E';
 
-            int[,] pos = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
-
-
             while (true)
             {
                 Console.Clear();
@@ -193,99 +201,92 @@
 
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 
-                
                 if (keyInfo.Key == ConsoleKey.UpArrow)
                 {
-                    if (player_y - 1 < 0 || player_y - 1 >= MIRO_SIZE)
+                    if (!IsWall(0, player_y, player_x))
                         continue;
 
-                    if (miro[player_y - 1, player_x] == '#')
-                        continue;
-                    else if(miro[player_y - 1, player_x] == 'E')
-                    {
-                        Console.WriteLine("도착!");
-                        break;
-                    }
-                    miro[player_y, player_x] = ' ';
-                    player_y--;
-                    miro[player_y, player_x] = 'P';
+                    MovePos(0, ref player_y, ref player_x);
                 }
                 else if (keyInfo.Key == ConsoleKey.DownArrow)
                 {
-                    if (player_y + 1 < 0 || player_y + 1 >= MIRO_SIZE)
+                    if (!IsWall(1, player_y, player_x))
                         continue;
 
-                    if (miro[player_y + 1, player_x] == '#')
-                        continue;
-                    else if (miro[player_y + 1, player_x] == 'E')
-                    {
-                        Console.WriteLine("도착!");
-                        break;
-                    }
-
-                    miro[player_y, player_x] = ' ';
-                    player_y++;
-                    miro[player_y, player_x] = 'P';
+                    MovePos(1, ref player_y, ref player_x);
                 }
                 else if (keyInfo.Key == ConsoleKey.LeftArrow)
                 {
-                    if (player_x - 1 < 0 || player_x - 1 >= MIRO_SIZE)
+                    if (!IsWall(2, player_y, player_x))
                         continue;
 
-                    if (miro[player_y, player_x - 1] == '#')
-                        continue;
-                    else if (miro[player_y, player_x - 1] == 'E')
-                    {
-                        Console.WriteLine("도착!");
-                        break;
-                    }
-
-                    miro[player_y, player_x] = ' ';
-                    player_x--;
-                    miro[player_y, player_x] = 'P';
+                    MovePos(2, ref player_y, ref player_x);
                 }
                 else if (keyInfo.Key == ConsoleKey.RightArrow)
                 {
-                    if (player_x + 1 < 0 || player_x + 1 >= MIRO_SIZE)
+                    if (!IsWall(3, player_y, player_x))
                         continue;
 
-                    if (miro[player_y, player_x + 1] == '#')
-                        continue;
-                    else if (miro[player_y, player_x + 1] == 'E')
-                    {
-                        Console.WriteLine("도착!");
-                        break;
-                    }
+                    MovePos(3, ref player_y, ref player_x);
 
-                    miro[player_y, player_x] = ' ';
-                    player_x++;
-                    miro[player_y, player_x] = 'P';
                 }
 
+                if (player_x == MIRO_SIZE - 1 && player_y == MIRO_SIZE - 1)
+                {
+                    Console.WriteLine("도착!");
+                    break;
+                }
             }
 
         }
 
-
-        static void MovePos()
+        static void MovePos(int pos, ref int player_y, ref int player_x)
         {
+            int[] dr = { -1, 1, 0, 0 };
+            int[] dc = { 0, 0, -1, 1 };
 
+            miro[player_y, player_x] = ' ';
+            player_y += dr[pos];
+            player_x += dc[pos];
+            miro[player_y, player_x] = 'P';
         }
 
+        static bool IsWall(int pos, int player_y, int player_x)
+        {
+            // 위 아래 왼 오
+            int[] dr = { -1, 1, 0, 0 };
+            int[] dc = { 0, 0, -1, 1 };
+
+            //미로 크기 체크
+            if (player_y + dr[pos] < 0 || player_y + dr[pos] >= MIRO_SIZE || player_x + dc[pos] < 0 || player_x + dc[pos] >= MIRO_SIZE)
+                return false;
+
+            //벽 체크
+            if (miro[player_y + dr[pos], player_x + dc[pos]] == '#')
+                return false;
+            else
+                return true;
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("고르시오");
-            Console.WriteLine("1) 길찾기 2) 미로찾기");
-            string input = Console.ReadLine();
-            if (input == "1")
+            while (true)
             {
-                MapGraph();
+                Console.WriteLine("고르시오");
+                Console.WriteLine("1) 길찾기 2) 미로찾기");
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    MapGraph();
+                }
+                else if (input == "2")
+                {
+                    Miro();
+                }
+                else
+                    break;
             }
-            else if (input == "2")
-            {
-                Miro();
-            }
+            
         }
 
     }
