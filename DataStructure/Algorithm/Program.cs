0f3fd63c057cbd99e0d2f97f874031cfab11315c
dlzkap11 <dlzkap11@gmail.com>
2026-07-02 11:04:21 +0900

    [Feat] BFS 알고리즘 추가
    목표지점에 대한 최단거리를 찾는 알고리즘 구현

--- DataStructure/Algorithm/Program.cs
+++ DataStructure/Algorithm/Program.cs
@@ -1,249 +1,314 @@
-﻿namespace Sort
+﻿using Algorithm;
+using System.Data;
+
+namespace Sort
 {
     public static class Extension
     {
-        // <선택정렬>
-        // 데이터 중 가장 작은 값부터 하나씩 선택하여 정렬
-        // 시간복잡도 -  O(n²)
-        // 공간복잡도 -  O(1)
-        // 안정정렬   -  X
+        //모든 정렬은 오름차순을 기준으로
+
+        // 선택정렬
+        // 가장 작은 값을 찾고 앞에다가 넣기를 반복해서 정렬하는 방식
         public static void SelectionSort(this IList<int> list)
         {
-            for (int i = 0; i < list.Count; i++)
+            int cnt = 0;
+            while (cnt < list.Count)
             {
-                int minIndex = i;
-                for (int j = i; j < list.Count; j++)
+                int minIndex = cnt;
+                for (int i = cnt + 1; i < list.Count; i++)
                 {
-                    if (list[j] < list[minIndex]) minIndex = j;
+                    if (list[i] < list[minIndex])
+                    {
+                        minIndex = i;
+                    }
                 }
-
-                int temp = list[minIndex];
-                list[minIndex] = list[i];
-                list[i] = temp;
+                int temp = list[cnt];
+                list[cnt] = list[minIndex];
+                list[minIndex] = temp;
+                cnt++; // 다음 자리로 밀어주기
             }
         }
 
-        // <삽입정렬>
-        // 데이터를 하나씩 꺼내어 정렬된 자료 중 적합한 위치에 삽입하여 정렬
-        // 시간복잡도 -  O(n²), 16개 미만일때 O(n) 에 가까움
-        // 공간복잡도 -  O(1)
-        // 안정정렬   -  O
+
+        //삽입정렬
+        // 첫번째 값이랑 두번째 값을 비교해서 더 두번째값이 더크면 첫번째값의 오른쪽 작으면 왼쪽으로 넣으면서 정렬하는 방식
+        // 이후 3번째 값이랑 두번째값도 똑같이 비교 후 왼쪽에 있는 값이 크면 다음으로 오른쪽 값이 크면 둘의 위치를 바꾸기
+        // 2 6 8 4 3 1
+        // 2 6 4 8 3 1 // 여기서 반복할 수 있게 해야함
+        // 2 6 4 3 8 1
+        // 2 6 4 3 1 8
+        
         public static void InsertionSort(this IList<int> list)
         {
-            for (int i = 1; i < list.Count; i++)
+            for(int i = 1;  i < list.Count; i++)
             {
-                // 1. 데이터를 하나 꺼낸다.
-                int target = list[i];
-                // 2. 정렬된 자료 중 적합한 위치에 삽입한다.
-                // 2-3. 반복해서
-                for (int j = i; j >= 0; j--)
+                // 오른쪽 값이 왼쪽보다 크면 다음으로
+                for (int j = i; j > 0; j--)
                 {
-                    // 예외 상황 : 제일 작을 값이었을 때는 맨 앞에 배치
-                    if (j == 0)
+                    if (list[j - 1] < list[j])
                     {
-                        list[0] = target;
-                    }
-                    // 2-1. 앞 데이터와 비교해서
-                    else if (list[j - 1] <= target)
-                    {
-                        // 더이상 안밀어내도 될때까지 이동한다.
-                        list[j] = target;
                         break;
                     }
                     else
                     {
-                        // 2-2. 더 작으면 뒤로 밀어내고
+                        int temp = list[j];
                         list[j] = list[j - 1];
+                        list[j - 1] = temp;
+
                     }
                 }
-
-
             }
         }
-
-        // <버블정렬>
-        // 서로 인접한 데이터를 비교하여 정렬
-        // 시간복잡도 -  O(n²), 인덱스를 사용하지 않는 자료구조에도 적용할 수 있는 정렬 알고리즘
-        // 공간복잡도 -  O(1)
-        // 안정정렬   -  O
+        
+        
+        //버블정렬
+        // 서로 인접한 데이터끼리 비교해서 정렬
+        // 순차적으로 돌면서 두 데이터를 비교해서 왼쪽이 크면 바꾸고 아니면 그냥 다음으로 가고 이런식
+        // 처음 돌 때 가장 큰 값이 뒤로 가야함
         public static void BubbleSort(this IList<int> list)
         {
-            for (int i = 1; i < list.Count; i++)
+            for(int j = 1; j < list.Count; j++)
             {
-                for (int j = 0; j < list.Count - 1; j++)
+                for (int i = 1; i < list.Count; i++)
                 {
-                    if (list[j] > list[j + 1])
+                    if (list[i - 1] > list[i])
                     {
-                        int temp = list[j];
-                        list[j] = list[j + 1];
-                        list[j + 1] = temp;
+                        int temp = list[i];
+                        list[i] = list[i - 1];
+                        list[i - 1] = temp;
                     }
                 }
             }
+
         }
 
-        // <병합정렬>
-        // 데이터를 2분할하여 정렬 후 합병
-        // 데이터 갯수만큼의 추가적인 메모리가 필요
-        // 시간복잡도 -  O(nlogn)
-        // 공간복잡도 -  O(n)
-        // 안정정렬   -  O
+
+        //퀵정렬
+        public static void QuickSort(this IList<int> list) => QuickSort(list, 0, list.Count - 1);
+        private static void QuickSort(IList<int> list, int start, int end)
+        { 
+        }
+        //힙정렬
+        public static void HeapSort(IList<int> list)
+        {
+
+        }
+
+        //병합정렬
+        //배열 내 값들을 분할해서 각각 정렬하면서 다시 합치는 정렬 방법
+        // 일단 쪼개기 -> 더이상 못쪼갤때까지 배열길이가 1이 될 때까지
+        // 더이상 못 쪼개면 값들을 비교하면서 정렬
+        // 정렬된 리스트를 원래 배열로 보내주기
         public static void MergeSort(this IList<int> list) => MergeSort(list, 0, list.Count - 1);
         private static void MergeSort(IList<int> list, int start, int end)
         {
+            
             if (start == end)
                 return;
 
-            int middle = (start + end) / 2;
+            int middle;
+            middle = (start + end) / 2;
+
             MergeSort(list, start, middle);
-            MergeSort(list, middle + 1, end);
+            MergeSort(list, end, middle);
             Merge(list, start, middle, end);
         }
 
         private static void Merge(IList<int> list, int start, int middle, int end)
         {
-            List<int> sortedList = new List<int>();
-            int left = start;
-            int right = middle + 1;
+            List<int> temp = new List<int>();
 
-            while (left <= middle && right <= end)
+            int left = 0;
+            int right = 0;
+
+            while (left < middle && right < end)
             {
-                if (list[left] < list[right])
+                if (list[start + left] < list[middle + right])
                 {
-                    sortedList.Add(list[left]);
+                    temp.Add(list[start + left]);
                     left++;
                 }
                 else
                 {
-                    sortedList.Add(list[right]);
+                    temp.Add(list[middle + right]);
                     right++;
                 }
             }
 
-            if (left > middle)
+            //left right중 남은 것들 넣기
+            
+            //while ()
             {
-                for (int i = right; i <= end; i++)
+                if (left < middle)
                 {
-                    sortedList.Add(list[i]);
-                }
-            }
-            else // if (right > end)
-            {
-                for (int i = left; i <= middle; i++)
-                {
-                    sortedList.Add(list[i]);
-                }
-            }
-
-            for (int i = 0; i < sortedList.Count; i++)
-            {
-                list[start + i] = sortedList[i];
-            }
-        }
-
-        // <퀵정렬>
-        // 하나의 피벗을 기준으로 작은값과 큰값을 2분할하여 정렬
-        // 최악의 경우(피벗이 최소값 또는 최대값)인 경우 시간복잡도가 O(n²)
-        // 시간복잡도 -  평균 : O(nlogn)   최악 : O(n²)
-        // 공간복잡도 -  O(1)
-        // 안정정렬   -  X
-        public static void QuickSort(this IList<int> list) => QuickSort(list, 0, list.Count - 1);
-        private static void QuickSort(IList<int> list, int start, int end)
-        {
-            if (start >= end)
-            {
-                return;
-            }
-
-            int pivot = start;
-            int left = pivot + 1;
-            int right = end;
-
-            while (left <= right)
-            {
-                while (list[left] <= list[pivot] && left < right)
-                {
+                    temp.Add(list[start + left]);
                     left++;
-                }
-                while (list[right] > list[pivot] && left <= right)
-                {
-                    right--;
-                }
-
-                if (left < right)
-                {
-                    int temp = list[left];
-                    list[left] = list[right];
-                    list[right] = temp;
                 }
                 else
                 {
-                    int temp = list[pivot];
-                    list[pivot] = list[right];
-                    list[right] = temp;
-                    break;
+                    temp.Add(list[middle + right]);
+                    right++;
                 }
             }
+            
 
-            QuickSort(list, start, right - 1);
-            QuickSort(list, right + 1, end);
+            
+            for(int i = 0; i < list.Count; i++)
+            {
+                list[start + i] = temp[i];
+            }
+
         }
 
-        // <힙정렬>
-        // 힙을 이용하여 우선순위가 가장 높은 요소가 가장 마지막 요소와 교체된 후 제거되는 방법을 이용
-        // 배열에서 연속적인 데이터를 사용하지 않기 때문에 캐시 메모리를 효율적으로 사용할 수 없어 상대적으로 느림
-        // 시간복잡도 -  O(nlogn)
-        // 공간복잡도 -  O(1)
-        // 안정정렬   -  X
-        public static void HeapSort(IList<int> list)
-        {
-            MakeHeap(list);
-
-            for (int i = list.Count - 1; i > 0; i--)
-            {
-                int temp = list[0];
-                list[0] = list[i];
-                list[i] = temp;
-                Heapify(list, 0, i);
-            }
-        }
-
-        private static void MakeHeap(IList<int> list)
-        {
-            for (int i = list.Count / 2 - 1; i >= 0; i--)
-            {
-                Heapify(list, i, list.Count);
-            }
-        }
-
-        private static void Heapify(IList<int> list, int index, int size)
-        {
-            int left = index * 2 + 1;
-            int right = index * 2 + 2;
-            int max = index;
-            if (left < size && list[left] > list[max])
-            {
-                max = left;
-            }
-            if (right < size && list[right] > list[max])
-            {
-                max = right;
-            }
-
-            if (max != index)
-            {
-                int temp = list[index];
-                list[index] = list[max];
-                list[max] = temp;
-                Heapify(list, max, size);
-            }
-        }
     }
 
     internal class Program
     {
+        class Pos
+        {
+            public Pos(int y, int x) { Y = y; X = x; }
+            public int Y;
+            public int X;
+        }
+
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+
+        void BFS()
+        {
+            int[] deltaY = new int[] { -1, 0, 1, 0 };
+            int[] deltaX = new int[] { 0, -1, 0, 1 };
+
+
+            bool[,] found = new bool[_board.Size, _board.Size];
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+
+
+            Queue<Pos> q = new Queue<Pos>();
+            q.Enqueue(new Pos(PosY, PosX));
+            found[PosY, PosX] = true;
+            parent[PosY, PosX] = new Pos(PosY, PosX);
+
+            while (q.Count > 0)
+            {
+                Pos pos = q.Dequeue();
+                int nowY = pos.Y;
+                int nowX = pos.X;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int nextY = nowY + deltaY[i];
+                    int nextX = nowX + deltaX[i];
+
+
+                    if (nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size) //보드사이즈보다 크거나 작은 값에 대한 예외처리
+                        continue;
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                        continue;
+                    if (found[nextY, nextX])
+                        continue;
+
+                    q.Enqueue(new Pos(nextY, nextX));
+                    found[nextY, nextX] = true;
+                    parent[nextY, nextX] = new Pos(nowY, nowX);
+                }
+            }
+
+            CalcPathFromParent(parent);
+        }
+
+        struct Node
+        {
+            public int Row;
+            public int Col;
+            public Node(int row, int col)
+            {
+                this.Row = row;
+                this.Col = col;
+            }
+            
+        }
+
+        static int[,] Map =
+        {   //Map[y, x]
+            { 0,0,0,0,0},
+            { 0,1,1,1,1},
+            { 0,1,0,0,0},
+            { 0,1,0,1,1},
+            { 0,0,0,1,0}
+        };
+        // 왼 오 아 위
+        static int[] directRow = { -1, 1, 0, 0 };
+        static int[] directColumn = { 0, 0, 1, -1 };
+
+        static int BFSPath(int startR, int startC, int endR, int endC)
+        {
+            // 시작점에서 도착점까지의 최단거리를 저장해두기
+            // 한칸씩 탐색하면서 현재까지의 거리 저장
+            // 도착하면 해당 변수의 내용이 최단거리고 그걸 리턴하면 되나?
+
+            //맵 크기
+            int mapRows = Map.GetLength(0);
+            int mapCols = Map.GetLength(1);
+
+            // 최단거리 방문체크 = -1, 못가는 곳(벽) = INF 거리 = 0++
+            int[,] dist = new int[mapRows, mapCols];
+            for (int i = 0; i < mapRows; i++) 
+            {
+                for (int j = 0; j < mapCols; j++) 
+                {
+                    dist[i, j] = -1;
+                }
+            }
+
+            Queue<Node> bfsQueue = new Queue<Node>();
+            bfsQueue.Enqueue(new Node(startR ,startC));
+            dist[startR, startC] = 0;
+
+            while(bfsQueue.Count > 0)
+            {
+                Node currentNode = bfsQueue.Dequeue();
+
+                int currentRow = currentNode.Row;
+                int currentCol = currentNode.Col;
+
+                if(currentRow == endR && currentCol == endC)
+                    return dist[currentRow, currentCol];
+
+                for(int i = 0; i < 4; i++)
+                {
+                    int moveRow = currentRow + directRow[i];
+                    int moveCol = currentCol + directColumn[i];
+
+                    if (moveRow < 0 || moveCol < 0 || moveRow >= mapRows|| moveCol >= mapCols)
+                        continue;
+                    if (Map[moveRow, moveCol] == 1 || dist[moveRow, moveCol] != -1)
+                        continue;
+
+                    dist[moveRow, moveCol] = dist[currentRow, currentCol] + 1;
+                    bfsQueue.Enqueue(new Node(moveRow, moveCol));
+
+                }
+
+            }
+
+
+            return -1;
+        }
+
+
+
+
         static void Main(string[] args)
         {
+            int result = BFSPath(0, 0, 2, 4);
+            Console.WriteLine($"도착지점까지의 최단거리 {result}");
+
+
+
+
+            /*
             Random random = new Random();
             int count = 10;
 
@@ -317,6 +382,40 @@
                 Console.Write($"{value,3}");
             }
             Console.WriteLine();
+
+            List<Monster> monsters = new List<Monster>();
+            monsters.Add(new Monster("왕뚜껑", 2, "라면"));
+            monsters.Add(new Monster("킹뚜껑", 5, "라면"));
+            monsters.Add(new Monster("밥아저씨", 18, "사람"));
+            monsters.Add(new Monster("그랜드마더", 12, "노인"));
+
+
+
+            SortWork.SortByAttribute(monsters);
+            Console.WriteLine("속성기준 정렬");
+            foreach (Monster monster in monsters)
+            {
+                Console.Write($"{monster.Name}, {monster.Level}, {monster.Attribute}");
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n레벨기준 정렬");
+            SortWork.SortByLevel(monsters);
+            foreach (Monster monster in monsters)
+            {
+                Console.Write($"{monster.Name}, {monster.Level}, {monster.Attribute}");
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n이름기준 정렬");
+            SortWork.SortByName(monsters);
+            foreach (Monster monster in monsters)
+            {
+                Console.Write($"{monster.Name}, {monster.Level}, {monster.Attribute}");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+
+            */
         }
     }
 }
---
+++ DataStructure/Algorithm/SortWork.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    public class Monster
+    {
+        public string Name;
+        public int Level;
+        public string Attribute;
+
+        public Monster(string name, int level, string attribute)
+        {
+            Name = name;
+            Level = level;
+            Attribute = attribute;
+        }
+
+    }
+
+
+    public static class SortWork
+    {
+
+        public static void SortByName(List<Monster> monsters)
+        {
+            monsters.Sort((x, y)=> x.Name.CompareTo(y.Name));
+        }
+
+        public static void SortByLevel(List<Monster> monsters)
+        {
+            // 오름차순 아마도
+            //monsters.Sort((x, y) => x.Level.CompareTo(y.Level));
+            //monsters.Sort((x, y) => x.Level - y.Level);
+
+            //내림차순
+            monsters.Sort((x, y) => y.Level - x.Level);
+        }
+
+        public static void SortByAttribute(List<Monster> monsters)
+        {
+            monsters.Sort((x, y) => x.Attribute.CompareTo(y.Attribute));
+        }
+
+    }
+}
